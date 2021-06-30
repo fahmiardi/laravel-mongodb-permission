3eb61224762a33855963d4dd5d19df38d94d95f3
B L Praveen <praveen.bl@heptagon.in>
2021-06-30 09:39:26 +0530

    Update

--- src/PermissionRegistrar.php
+++ src/PermissionRegistrar.php
@@ -22,7 +22,7 @@ class PermissionRegistrar extends BasePermissionRegistrar
      *
      * @return \Moloquent\Eloquent\Collection
      */
-    public function getPermissions()
+    public function getPermissions(array $params = Array)
     {
         return app(Permission::class)->get();
     }
